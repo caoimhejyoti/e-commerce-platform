d87451af33cdd414b74bbe59bcd93c27a4e1a9e1
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-22 11:06:31 +0800

    adding delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -121,8 +121,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData[0]) {
+      res.status(404).json({ message: 'No Product with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
